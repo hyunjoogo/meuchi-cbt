b66fe38511eb63ab4ac851a6cb9361aa0fee3528
HyunJoo Kim <hyunjoogo@gmail.com>
2022-02-22 14:45:43 +0900

    회원가입 페이지 완성

--- prepare/front/pages/signup.js
+++ prepare/front/pages/signup.js
@@ -4,9 +4,8 @@ import AppLayout from "../components/AppLayout";
 import {Button, Col, Container, Form, Row} from "react-bootstrap";
 
 const Signup = () => {
-  const {register, handleSubmit, watch, formState: {errors}} = useForm();
+  const {register, handleSubmit, watch, getValues, formState: {errors}} = useForm();
   const onSubmit = data => {
-    console.log(errors)
     console.log(data);
   }
 
@@ -29,8 +28,58 @@ const Signup = () => {
                 },
               })}
             />
+            <div>{errors?.email?.message}</div>
           </Form.Group>
-          <div>{errors?.email?.message}</div>
+          <Form.Group controlId="name">
+            <Form.Label column="sm">이름</Form.Label>
+            <Form.Control
+              {...register("name", {
+                required: "이름을 입력해주세요.",
+                maxLength: {
+                  value: 256,
+                  message: "256자 이내로 입력해주세요."
+                },
+              })}
+              placeholder="이름을 입력해주세요."
+            />
+            <div>{errors?.name?.message}</div>
+          </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Label column="sm">비밀번호</Form.Label>
+            <Form.Control
+              {...register("password", {
+                required: "비밀번호를 입력해주세요.",
+                minLength: 8,
+                maxLength: {
+                  value: 16,
+                  message: "8~16자리 영문자, 숫자, 특수문자를 사용해주세요."
+                },
+                pattern: {
+                  value: /^(?=.*[a-zA-Z])((?=.*\d)(?=.*\W)).{8,16}$/,
+                  message: "영문자, 숫자, 특수문자를 사용해주세요.",
+                },
+              })}
+              placeholder="8~16자리 영문자, 숫자, 특수문자를 사용해주세요." type="password"
+            />
+            <div>{errors?.password?.message}</div>
+          </Form.Group>
+          <Form.Group controlId="confirmPassword">
+            <Form.Label column="sm">비밀번호 확인</Form.Label>
+            <Form.Control
+              {...register("confirmPassword", {
+                required: "비밀번호를 입력해주세요.",
+                validate: value => {
+                  if (value === getValues('password')) {
+                    return true
+                  } else {
+                    return <span>비밀번호가 맞지 않습니다.</span>
+                  }
+                }
+              })}
+              placeholder="다시 한 번 입력해주세요." type="password"
+            />
+            <div>{errors?.confirmPassword?.message}</div>
+          </Form.Group>
           <Button variant="primary" size="sm" type="submit">저장</Button>
         </Form>
       </Container>
